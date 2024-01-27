de2b560d38e3b6254783e68466f6621da423e0c0
JeanL-coder <moralesjean471@gmail.com>
2024-01-27 11:17:54 -0400

    Modificaciones en los componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './vistas/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path:'',
+    redirectTo:'login', 
+    pathMatch:'full'
+  },
+  {
+    path:'',
+    component:LoginComponent
+  }
+];
--- src/app/vistas/registro/registro.component.ts
+++ src/app/vistas/registro/registro.component.ts
@@ -1,12 +1,57 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
 export class RegistroComponent {
 
+  userEmail: string = '';
+  userPassword: string = '';
+  emailInvalid: boolean = false;
+  passwordInvalid: boolean = false;
+
+  onSubmit() {
+    // Validar el correo electrónico y la contraseña
+    if (this.validateEmail(this.userEmail) && this.validatePassword(this.userPassword)) {
+      // Ambos son válidos, puedes realizar otras acciones aquí
+      console.log('Correo y contraseña válidos. Realizar acciones de registro.');
+    } else {
+      // Al menos uno de ellos no es válido
+      if (!this.validateEmail(this.userEmail)) {
+        this.emailInvalid = true;
+      }
+
+      if (!this.validatePassword(this.userPassword)) {
+        this.passwordInvalid = true;
+      }
+    }
+  }
+
+  // Función para validar el correo electrónico
+  validateEmail(email: string): boolean {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  // Función para validar la contraseña
+  validatePassword(password: string): boolean {
+    // Verificar longitud mínima
+    if (password.length < 5) {
+      return false;
+    }
+
+    // Verificar presencia de letras mayúsculas, minúsculas y números
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+
+    return hasUpperCase && hasLowerCase && hasNumber;
+  }
 }
+
